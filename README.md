0628b8b011ac618b6068be18b53b49cc07401f60
niiputuekaadwiyantii <162816040+niiputuekaadwiyantii@users.noreply.github.com>
2024-03-10 15:11:14 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# FileAnalisis
+
+# Bike Rentals - Final Project for Data Analytics with Python Dicoding
+
+This serves as the concluding assignment for the 'Learn Data Analysis with Python' course on Dicoding, where I utilized the bike sharing dataset to conduct analysis and develop a dashboard. Within the notebook, I've documented the process of Data Wrangling, Exploratory Data Analysis, and Data Visualization. Additionally, I've employed Streamlit to craft the dashboard.
+
+
+## 1. File Structures
+```
+.
+├── dashboard
+│   ├── dashboard.py
+│   └── day.csv
+├── data
+│   ├── Readme.txt
+│   ├── day.csv
+|   └── hour.csv
+├── screenshots
+|   ├── Screenshots (91).png
+|   ├── Screenshots (92).png
+|   ├── Screenshots (93).png
+|   └── Screenshots (94).png
+├── README.md
+├── notebook.ipynb
+└── requirements.txt
+```
+
+## 2. Project work steps
+1. Data Wrangling: 
+ - Gathering data
+ - Assessing data
+ - Cleaning data
+2. Exploratory Data Analysis:
+ - Defined business questions for data exploration
+ - Create Data exploration
+3. Data Visualization:
+ - Create Data Visualization to answer business questions
+4. Dashboard:
+ - Set up the DataFrame which will be used
+ - Make filter components on the dashboard
+ - Complete the dashboard with data visualizations
+
+
+## 3. Starting
+### `notebook.ipynb`
+1. Download this project.
+2. Open your IDE like Jupyter Notebook or Google Colaboratory.
+3. Create a New Notebook.
+4. Upload and select the file with .ipynb extension.
+5. Connect to hosted runtime.
+6. And run the code cells.
+
+### `dashboard/dashboard.py`
+1. Download this project.
+2. Install the Streamlit in your terminal or command prompt using `pip install streamlit`.
+3. Install libraries like pandas, numpy, plotly, matplotlib, and seaborn if you haven't.
+3. Please keep in note that do not move the csv file because it acts a data source. keep it in one folder as dashboard.py
+4. Open your VSCode and run the file by clicking the terminal and write it `streamlit run dashboard.py` in your terminal.
+
+## 4. Screenshots
+
+![alt text](https://github.com/niiputuekaadwiyantii/dashboard/blob/a155e3198fa5a33d1d0fc85a99143325f697a286/WhatsApp%20%E5%9C%96%E7%89%87%E5%84%B2%E5%AD%98%E6%99%82%E9%96%932024-03-10%EF%BC%8C14.57.32_cdede9a5.jpg)
+
+![alt text](https://github.com/niiputuekaadwiyantii/dashboard/blob/main/WhatsApp%20%E5%9C%96%E7%89%87%E5%84%B2%E5%AD%98%E6%99%82%E9%96%932024-03-10%EF%BC%8C14.57.38_b3457f1f.jpg)
+
+![alt text](https://github.com/niiputuekaadwiyantii/dashboard/blob/main/WhatsApp%20%E5%9C%96%E7%89%87%E5%84%B2%E5%AD%98%E6%99%82%E9%96%932024-03-10%EF%BC%8C14.57.49_0f81adcb.jpg)
---
+++ dashboard.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from datetime import datetime
+import streamlit as st
+
+# Load or define data
+day_df = pd.read_csv("all_data.csv")
+
+#Filter component
+min_date = pd.to_datetime(day_df['dateday']).dt.date.min()
+max_date = pd.to_datetime(day_df['dateday']).dt.date.max()
+ 
+with st.sidebar:
+    st.image('EKA Bike.png')
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value= min_date,
+        max_value= max_date,
+        value=[min_date, max_date]
+    )
+
+main_df = day_df[(day_df['dateday'] >= str(start_date)) & 
+                (day_df['dateday'] <= str(end_date))]
+
+def create_user_weather(df):
+    result = df.groupby(by='weather_cond').agg({
+        'count': ['max', 'min', 'mean', 'sum']
+    })
+    return result
+
+def create_user_year(df):
+    df['month'] = pd.Categorical(df['month'], categories=
+    ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    ordered=True)
+    monthly_counts = df.groupby(by=["month","year"]).agg({
+        "count": "sum"
+    }).reset_index()
+    return monthly_counts
+
+def main():
+    st.header('Projek Analisis Data: Bike Sharing')
+    st.subheader('Course: Analysis Data with Python')
+    st.subheader('Pengaruh cuaca terhadap jumlah pengguna sepeda')
+    user_weather = create_user_weather(day_df)
+    
+    plt.figure(figsize=(8,6))
+    sns.barplot(
+        x='weather_cond',
+        y='count',
+        data=day_df
+    )
+
+    plt.title('Jumlah Pengguna Sepeda berdasarkan Cuaca')
+    plt.xlabel('Kondisi Cuaca')
+    plt.ylabel('Jumlah Pengguna Sepeda')
+    st.pyplot(plt.gcf())  # Pass the current figure to st.pyplot()
+
+    st.subheader('Perbadingan jumlah pengguna sepeda pada tahun 2011 vs 2012')
+    comparison_year = create_user_year(day_df)
+    sns.lineplot(
+        data=comparison_year,
+        x="month",
+        y="count",
+        hue="year",
+        palette="rocket",
+        marker="o"
+    )
+
+    plt.title("Jumlah pengguna sepeda berdasarkan Bulan dan tahun")
+    plt.xlabel(None)
+    plt.ylabel(None)
+    plt.legend(title="Tahun", loc="upper right")
+    plt.tight_layout()
+    st.pyplot(plt.gcf())  # Pass the current figure to st.pyplot()
+
+    st.subheader('Pengaruh suhu atau temperatur serta kelembapan terhadap pengguna sepeda baik casual dan registered')
+    plt.figure(figsize=(10, 6))
+
+    plt.subplot(1, 3, 1)
+    sns.scatterplot(
+        x='temp',
+        y='count',
+        data=day_df,
+        alpha=0.5
+    )
+    plt.title('Temperature vs Count')
+
+    plt.subplot(1, 3, 2)
+    sns.scatterplot(
+        x='atemp',
+        y='count',
+        data=day_df,
+        alpha=0.5
+    )
+    plt.title('Feels Like Temperature vs Count')
+
+    plt.subplot(1, 3, 3)
+    sns.scatterplot(
+        x='hum',
+        y='count',
+        data=day_df,
+        alpha=0.5
+    )
+    plt.title('Humidity vs Count')
+
+    st.pyplot(plt.gcf())  # Pass the current figure to st.pyplot()
+
+if __name__ == '__main__':
+    main()
